a1c2f77b18b8eaa0e295bf5a1a07c5959441c4b3
klikknpro <klikknpro@gmail.com>
2021-10-21 08:33:44 +0200

    Testing multiple (not working yet)

--- countries/countries.js
+++ countries/countries.js
@@ -1,8 +1,13 @@
 window.addEventListener("load", loadCountry);
 
-function loadCountry() {
-  const search = "brazil";
-  fetch(`https://restcountries.com/v3.1/name/${search}`)
+const searchList = ["brazil"];
+for (let i = 0; i < searchList.length; i++) {
+  const searchLink = `https://restcountries.com/v3.1/name/${searchList[i]}`;
+  loadCountry(searchLink);
+}
+
+function loadCountry(link) {
+  fetch("link")
     .then((r) => r.json())
     .then(showCountry);
 }
@@ -11,8 +16,8 @@ function showCountry(c) {
   console.log(c);
   const name = c[0].name.official;
   const flag = c[0].flags.png;
-  document.getElementById("flag").setAttribute("src", flag);
   document.getElementById("name").textContent = name;
+  document.getElementById("flag").setAttribute("src", flag);
 }
 
 /* list more countries
